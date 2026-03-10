aa4477dbdc5a45b53002831ed1d9ae39f8b3a90b
Aryan Saini <aryanofficial8380@gmail.com>
2026-03-10 14:02:51 +0530

    UC6: Refactored banner rendering into static methods in Java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,73 @@
 /**
  * OOPSBannerApp
- * Use Case 5 (UC5): Array Initialisation
+ * Use Case 6 (UC6): Static Functions
  * 
  * @author Aryan Saini
- * @version 5.0
+ * @version 6.0
  */
 public class OOPSBannerApp {
 
+    /**
+     * Helper method to generate letter O
+     */
+    public static String[] getO() {
+        return new String[] {
+            "*****",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*****"
+        };
+    }
+
+    /**
+     * Helper method to generate letter P
+     */
+    public static String[] getP() {
+        return new String[] {
+            "*****",
+            "*   *",
+            "*   *",
+            "*****",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    /**
+     * Helper method to generate letter S
+     */
+    public static String[] getS() {
+        return new String[] {
+            "*****",
+            "*    ",
+            "*    ",
+            "*****",
+            "    *",
+            "    *",
+            "*****"
+        };
+    }
+
     public static void main(String[] args) {
 
-        // Array Declaration and Initialization in a single statement
-        String[] banner = {
-            String.join("   ", "*****", "*****", "*****", "*****"),
-            String.join("   ", "*   *", "*   *", "*   *", "*    "),
-            String.join("   ", "*   *", "*   *", "*   *", "*    "),
-            String.join("   ", "*   *", "*   *", "*****", "*****"),
-            String.join("   ", "*   *", "*   *", "*    ", "    *"),
-            String.join("   ", "*   *", "*   *", "*    ", "    *"),
-            String.join("   ", "*****", "*****", "*    ", "*****")
-        };
+        String[] charO1 = getO();
+        String[] charO2 = getO();
+        String[] charP = getP();
+        String[] charS = getS();
+
+        String[] bannerLines = new String[7];
+        
+        // Loop-based rendering logic
+        for (int i = 0; i < 7; i++) {
+            bannerLines[i] = String.join("   ", charO1[i], charO2[i], charP[i], charS[i]);
+        }
 
         // Enhanced for loop to print the banner
-        for (String line : banner) {
+        for (String line : bannerLines) {
             System.out.println(line);
         }
     }
